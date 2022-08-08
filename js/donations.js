592e1ab53940ea660274e1f3fc3dd413b1b00ec1
steph-crown <emmanuelstephen024@gmail.com>
2022-08-08 07:44:06 +0100

    Create manifesto page

--- js/donations.js
+++ js/donations.js
@@ -1,9 +1,9 @@
-const PLK = "sk_test_5f55171caf4eb07154eeaa55558a112634396c46";
+const SLK = "sk_test_5f55171caf4eb07154eeaa55558a112634396c46";
 
 const myHeaders = new Headers();
 myHeaders.append(
   "authorization",
-  "Bearer sk_test_5f55171caf4eb07154eeaa55558a112634396c46"
+  "Bearer sk_live_a04eca368733f073cfa5ee7a4be78eed707ee138"
 );
 // myHeaders.append(
 //   "Cookie",
